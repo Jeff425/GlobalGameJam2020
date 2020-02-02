14a0c1a23e5b137b221646dfc3bf3b3af50ecbf7
Jeff Goodhue <a>
2020-02-02 01:56:33 -0800

    Added rotation prefabs, began working on swapping prefabs. Animation has issues

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -9,6 +9,11 @@ public class ButtonController : MonoBehaviour
 
     [SerializeField]
     Colors[] affectedColors;
+    Animator animator;
+
+    void Awake() {
+        animator = transform.GetChild(0).GetComponent<Animator>();
+    }
 
     public void Trigger() {
         if (!rotate) {
@@ -17,11 +22,16 @@ public class ButtonController : MonoBehaviour
             Direction colorB = GameVariables.GetColor(affectedColors[1]);
             GameVariables.SetColor(affectedColors[0], colorB);
             GameVariables.SetColor(affectedColors[1], colorA);
+            GameController.Instance.RotateTilesOfColor(affectedColors[0]);
+            GameController.Instance.RotateTilesOfColor(affectedColors[1]);
+            animator.SetBool("IsSwapping", true);
+            return;
         }
         foreach(Colors color in affectedColors) {
             GameVariables.SetColor(color, Rotate90(GameVariables.GetColor(color)));
             GameController.Instance.RotateTilesOfColor(color);
         }
+        animator.SetBool("IsRotating", true);
     }
 
     Direction Rotate90(Direction inDirection) {
